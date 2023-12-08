780ae75182b976c9aeb693a9e3083558da772a8e
Brianca Knight <briancaknight@gmail.com>
2023-12-08 11:00:15 -0800

    create ItemControl.js and update App.js to print ItemControl

---
+++ src/components/ItemControl.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import NewItemForm from './NewItemForm';
+import ItemList from './ItemList';
+
+class ItemControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+    };
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}
+
+export default ItemControl;
