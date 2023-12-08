ef050b3455f4282ff3ed90924504b5414e58cb9d
Brianca Knight <briancaknight@gmail.com>
2023-12-08 10:12:34 -0800

    declare prop types

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Item(){
+function Item(props){
   return(
     <React.Fragment>
-      
+      <p>{props.name}{props.price}{props.quantity}</p>
     </React.Fragment>
   );
+}
+
+Item.propTypes = {
+  name: PropTypes.string,
+  price: PropTypes.number,
+  quantity: PropTypes.number,
 }
 
 export default Item;
