d0a6c21000227f4cf38ba9f8972092c8baac5a21
Brianca Knight <briancaknight@gmail.com>
2023-12-08 10:49:10 -0800

    add ReusableForm.js

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -2,11 +2,28 @@ import React from 'react';
 import Item from './Item.js';
 import PropTypes from 'prop-types';
 
+const mainItemList = [
+  {
+    names: 'Super Shampoo',
+    price: '1.25',
+    quantity: '640'
+  },
+  {
+    names: 'Bubbly Body Wash',
+    price: '1',
+    quantity: '640'
+  },
+  {
+    names: 'Dazling Dish Soap',
+    price: '.75',
+    quantity: '640'
+  },
+]
+
 function ItemList(props) {
   return (
     <React.Fragment>
-      <hr />
-      {props.ItemList.map((item, index) => 
+      {props.mainItemList?.map((item, index) => 
       <Item
       name={item.name}
       price={item.price}
---
+++ src/components/ReusableForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ReusableForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Item name' />
+        <input
+          type='text'
+          name='price'
+          placeholder='Enter price per oz' />
+        <input
+          type='text'
+          name='quantity'
+          placeholder='Enter quantity in oz' />
+          <button type='submit'>{props.buttonText}</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+ReusableForm
