a23a668f31ae83d235a714f5c140428714053222
Brianca Knight <briancaknight@gmail.com>
2023-12-08 10:31:24 -0800

    declare prop types and add loop

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import Item from 'Item.js';
+import Item from './Item.js';
+import PropTypes from 'prop-types';
 
-function ItemList(){
-  return(
-    <Item />
+function ItemList(props) {
+  return (
+    <React.Fragment>
+      <hr />
+      {props.ItemList.map((item, index) => 
+      <Item
+      name={item.name}
+      price={item.price}
+      quantity={item.quantity}
+      id={item.id}
+      key={item.id} />
+      )}
+    </React.Fragment>
   );
+}
+
+ItemList.propTypes = {
+  itemList: PropTypes.array,
 }
 
 export default ItemList;
