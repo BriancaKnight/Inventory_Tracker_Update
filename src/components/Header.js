55a81f36ddedf153df07b110df9e0be46c0c33cd
Brianca Knight <briancaknight@gmail.com>
2023-12-08 09:43:39 -0800

    add header text, fix app.js and index.js pathing issues.

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+  return (
+    <>
+    <h1>The Refillery</h1>
+    <h3>Your one-stop-shop for all your refillable household needs!</h3>
+    </>
+  );
+}
+
+export default Header;
