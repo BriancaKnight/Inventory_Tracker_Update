c45c0ba9ea9c7fc29a831fb234290cd619ada446
Brianca Knight <briancaknight@gmail.com>
2023-12-08 16:31:12 -0800

    adjust quanity

--- src/components/CoreItems.js
+++ src/components/CoreItems.js
@@ -4,19 +4,19 @@ let CoreItems = [
   {
     name: 'Super Shampoo',
     price: '1.25',
-    quantity: '640',
+    quantity: '128',
     description: 'Suds up with the worlds best shampoo!'
   },
   {
     name: 'Bubbly Body Wash',
     price: '1',
-    quantity: '640',
+    quantity: '128',
     description: 'Guaranteed to get you sparkling'
   },
   {
     name: 'Dazling Dish Soap',
     price: '.75',
-    quantity: '640',
+    quantity: '128',
     description: 'So clean you could eat off of it!'
   },
 ]
